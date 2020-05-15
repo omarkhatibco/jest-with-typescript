53e236a1f2e250f208913ff1737ba7ecaf2b2d04
Omar Khatib <omar@omarkhatib.co>
2020-05-15 14:51:33 +0200

    maybe bug

--- jest.config.js
+++ jest.config.js
@@ -4,13 +4,11 @@ module.exports = {
     "!**/*.d.ts",
     "!**/node_modules/**",
   ],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testPathIgnorePatterns: ["/node_modules/", "/.next/"],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
   },
   moduleDirectories: [".", "node_modules"],
   transformIgnorePatterns: ["/node_modules/"],
-  // moduleNameMapper: {
-  // 	'^@(.*)$': '<rootDir>$1',
-  // },
 };
